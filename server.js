454c5ec969079266f935db1d8dd77e174e95b87d
lianwangtao <wlian3@wisc.edu>
2017-03-01 19:14:07 -0600

    Add base64-image

--- server.js
+++ server.js
@@ -2,8 +2,13 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const base64 = require('node-base64-image')
 
 app.get('/', (request, response) => {
+  const url = 'http://upload.wikimedia.org/wikipedia/commons/4/4a/Logo_2013_Google.png'
+  base64.encode(url, {string: 'true'}, function(response) {
+    console.log('dataURL:', response)
+  })
   response.send('Hello from Express!')
 })
 
