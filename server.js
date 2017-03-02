927d10fce48154aa11ecf3f89517db159a2f8597
lianwangtao <wlian3@wisc.edu>
2017-03-01 22:57:34 -0600

    Update server with parser

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
 // https://young-meadow-70227.herokuapp.com/
-const express = require('express')
-const app = express()
-const port = 3000
-const base64 = require('node-base64-image')
+var express = require('express')
+var base64 = require('node-base64-image')
+var bodyParser = require('body-parser')
 
-app.get('/', (request, response) => {
-  const url = 'http://upload.wikimedia.org/wikipedia/commons/4/4a/Logo_2013_Google.png'
-  base64.encode(url, {string: 'true'}, function(response) {
-    console.log('dataURL:', response)
-  })
-  response.send('Hello from Express!')
+const PORT = process.env.port || 3000
+const app = express()
+
+app.use(bodyParser.json())
+
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "X-Requested-With")
+  next()
 })
 
-app.listen(port, (err) => {
+app.post('/', (req, res) => {
+  const url = req.body.imageUrl
+  var base64Image = new Buffer(url, 'binary').toString('base64')
+  var result = 'data:image/jpeg;base64,' + base64Image
+  res.status(200).send(result)
+})
+
+app.listen(PORT, (err) => {
   if (err) {
     return console.log('something bad happened', err)
   }
-
-  console.log(`server is listening on ${port}`)
+  console.log(`server is listening on ${PORT}`)
 })
